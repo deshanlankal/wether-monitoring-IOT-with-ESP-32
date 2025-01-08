27f71e202bb3530767c4846e8512774ed3ec54c8
Deshan Lanka <deshanlanka500@gmail.com>
2025-01-08 14:32:36 +0530

    feat: add wind direction and speed display to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,8 +17,7 @@ import { createClient } from "@supabase/supabase-js";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { loginTb } from "@/actions";
-import { readSetting } from "@/actions";
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
@@ -33,6 +32,8 @@ export function Footer() {
   const [rainfall, setRainfall] = useState(0);
   const [uvIndex, setUvIndex] = useState(0);
   const [airQuality, setAirQuality] = useState(0);
+  const [windDirection, setWindDirection] = useState("NA");
+  const [windSpeed, setWindSpeed] = useState(0);
 
   useEffect(() => {
     const fetchWeatherData = async () => {
@@ -58,6 +59,8 @@ export function Footer() {
           setRainfall(latestData.rainfall);
           setUvIndex(latestData.uvIndex);
           setAirQuality(latestData.airQuality);
+          setWindDirection(latestData.windDirection);
+          setWindSpeed(latestData.windSpeed);
         }
       } catch (error) {
         console.error("Unexpected error:", error);
@@ -69,6 +72,26 @@ export function Footer() {
 
   return (
     <div className="flex flex-col">
+      <div className="flex justify-between mx-4">
+        <div className="flex flex-col">
+          <div className="flex gap-3">
+            <h3 className="text-2xl">Wind</h3>
+            <Icons.wind className="h-8 w-8 rounded-full bg-[#0e1426] p-1" />
+          </div>
+          <div className="flex gap-2">
+            <p className="text-xl text-[#5f6281]">Direction:</p>
+            <p className="text-xl text-white">{windDirection}</p>
+          </div>
+          <div className="flex gap-2">
+            <p className="text-xl text-[#5f6281]">Speed:</p>
+            <p className="text-xl text-white">{windSpeed} m/s</p>
+          </div>
+        </div>
+        <div className="flex items-end">
+          <p className="text-xl text-[#5f6281]">Moon Phase</p>
+          <Icons.moon className="h-12 w-12" />
+        </div>
+      </div>
       <div className="flex m-4 gap-3 min-h-60">
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Parallax]}
